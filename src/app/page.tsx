dfe8dd9daba0aec75aa4b34668c377a9877b97af
dyakovmaksim <pochta3478@mail.ru>
2024-10-10 14:13:05 +0500

    add top-bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,28 @@
+import { Container, Title, Categories, SortPopup, TopBar } from "@/components/shared";
 
 export default function home() {
-  return <h1>Главная</h1>
+  return <>
+    <Container className="mt-10">
+      <Title text="Все пиццы" size="lg" className="font-extrabold" />
+    </Container>
+
+    <TopBar/>
+
+    <Container className="pb-14">
+      <div className="flex gap-[60px]">
+
+        {/* ФИЛЬТРАЦИЯ */}
+        <div className="w-[250px]">
+        ФИЛЬТРАЦИЯ
+        </div>
+
+        {/* Список товаров */}
+        <div className="flex-1">
+            <div className="flex flex-col gap-16">
+              Список товаров
+            </div>
+        </div>
+      </div>
+    </Container>
+  </>
 }
